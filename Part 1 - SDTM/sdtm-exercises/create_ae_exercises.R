3ac53929f5841e2f92423848740456711dd26bb0
Shiyu Chen <chenshiyu2017qiu@hotmail.com>
2025-05-23 22:08:38 +0000

    SDTM exercises, answers and specs

---
+++ Part 1 - SDTM/sdtm-exercises/create_ae_exercises.R
@@ -0,0 +1,210 @@
+#' Name: AE domain
+#'
+#' Label: R program to create AE Domain
+#' 
+#' Input 
+#' raw data: pharmaverseraw::ae_raw
+#' study_controlled_terminology : sdtm_ct.csv
+#' dm domain : pharmaversesdtm::dm
+#'
+library(sdtm.oak)
+library(dplyr)
+
+
+# Read CT Specification
+study_ct <- read.csv("./Part 1 - SDTM/sdtm-specs/sdtm_ct.csv")
+
+# Read in raw data
+ae_raw <- pharmaverseraw::ae_raw
+
+ae_raw <- admiral::convert_blanks_to_na(ae_raw)
+
+# Derive oak_id_vars
+ae_raw <- ae_raw %>%
+  generate_oak_id_vars(
+    pat_var = ,
+    raw_src = 
+  )
+
+# Read in DM domain to derive study day
+dm <- pharmaversesdtm::dm
+
+dm <- admiral::convert_blanks_to_na(dm)
+
+# Create AE domain. The first step in creating AE domain is to create the topic variable
+ae <-
+  # Derive topic variable
+  # Map AETERM using assign_no_ct, raw_var=IT.AETERM, tgt_var=AETERM
+  assign_no_ct(
+    raw_dat = ae_raw,
+    raw_var = "IT.AETERM",
+    tgt_var = "AETERM",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AEOUT using assign_ct, raw_var=AEOUTCOME, tgt_var=AEOUT
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "AEOUTCOME",
+    tgt_var = "AEOUT",
+    ct_spec = study_ct,
+    ct_clst = "C66768",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AESEV using assign_no_ct, raw_var=IT.AESEV, tgt_var=AESEV
+  # assign_ct(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   ct_spec = ,
+  #   ct_clst = ,
+  #   id_vars = 
+  # )  %>%
+  # Map AESER using assign_no_ct, raw_var=IT.AESER, tgt_var=AESER
+  # assign_ct(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   ct_spec = ,
+  #   ct_clst = ,
+  #   id_vars = 
+  # ) %>%
+  # Map AEACN using assign_no_ct, raw_var=IT.AEACN, tgt_var=AEACN
+  # assign_no_ct(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   id_vars = 
+  # ) %>%
+  # Map AEREL using assign_ct, raw_var=IT.AEREL, tgt_var=AEREL
+  # User-added codelist is in the ct,
+  # assign_ct(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   ct_spec = ,
+  #   ct_clst = ,
+  #   id_vars = 
+  # ) %>%
+  # Map AESCAN using assign_ct, raw_var=AESCAN, tgt_var=AESCAN
+  # assign_ct(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   ct_spec = ,
+  #   ct_clst = ,
+  #   id_vars = 
+  # ) %>%
+  # Map AESCNO using assign_ct, raw_var=AESCNO, tgt_var=AESCNO
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "AESCNO",
+    tgt_var = "AESCONG",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AEDIS using assign_ct, raw_var=AEDIS, tgt_var=AEDIS
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "AEDIS",
+    tgt_var = "AESDISAB",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AESDTH using assign_ct, raw_var=IT.AESDTH, tgt_var=AESDTH
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "IT.AESDTH",
+    tgt_var = "AESDTH",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AESHOSP using assign_ct, raw_var=IT.AESHOSP, tgt_var=AESHOSP
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "IT.AESHOSP",
+    tgt_var = "AESHOSP",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AESLIFE using assign_ct, raw_var=IT.AESLIFE, tgt_var=AESLIFE
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "IT.AESLIFE",
+    tgt_var = "AESLIFE",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AESOD using assign_ct, raw_var=AESOD, tgt_var=AESOD
+  assign_ct(
+    raw_dat = ae_raw,
+    raw_var = "AESOD",
+    tgt_var = "AESOD",
+    ct_spec = study_ct,
+    ct_clst = "C66742",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AEDTC using assign_datetime, raw_var=AEDTCOL
+  # assign_datetime(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   raw_fmt = 
+  # ) %>%
+  # Map AESTDTC using assign_datetime, raw_var=IT.AESTDAT
+  # assign_datetime(
+  #   raw_dat = ,
+  #   raw_var = ,
+  #   tgt_var = ,
+  #   raw_fmt = ,
+  #   id_vars =
+  # ) %>%
+  # Map AEENDTC using assign_datetime, raw_var=IT.AEENDAT
+  assign_datetime(
+    raw_dat = ae_raw,
+    raw_var = "IT.AEENDAT",
+    tgt_var = "AEENDTC",
+    raw_fmt = c("m/d/y"),
+    id_vars = oak_id_vars()
+  ) %>%
+  dplyr::mutate(
+    STUDYID = ae_raw$STUDY,
+    DOMAIN = "AE",
+    USUBJID = paste0("01-", ae_raw$PATNUM),
+    AELLT = ae_raw$AELLT,
+    AELLTCD = ae_raw$AELLTCD,
+    AEDECOD = ae_raw$AEDECOD,
+    AEPTCD = ae_raw$AEPTCD,
+    AEHLT = ae_raw$AEHLT,
+    AEHLTCD = ae_raw$AEHLTCD,
+    AEHLGT = ae_raw$AEHLGT,
+    AEHLGTCD = ae_raw$AEHLGTCD,
+    AEBODSYS = ae_raw$AEBODSYS,
+    AEBDSYCD = ae_raw$AEBDSYCD,
+    AESOC = ae_raw$AESOC,
+    AESOCCD = ae_raw$AESOCCD,
+    AETERM = toupper(AETERM)
+  ) %>%
+  derive_seq(tgt_var = "AESEQ",
+             rec_vars= c("USUBJID", "AETERM")) %>%
+  # derive_study_day(
+  #   sdtm_in = .,
+  #   dm_domain = dm,
+  #   tgdt = ,
+  #   refdt = ,
+  #   study_day_var = 
+  # ) %>%
+  # derive_study_day(
+  #   sdtm_in = .,
+  #   dm_domain = dm,
+  #   tgdt = ,
+  #   refdt = ,
+  #   study_day_var = 
+  # ) %>%
+  select("STUDYID", "DOMAIN", "USUBJID", "AESEQ", "AETERM", "AELLT", "AELLTCD", "AEDECOD", "AEPTCD", "AEHLT", "AEHLTCD", "AEHLGT",
+         "AEHLGTCD", "AEBODSYS", "AEBDSYCD", "AESOC", "AESOCCD", "AESEV", "AESER", "AEACN", "AEREL", "AEOUT", "AESCAN", "AESCONG", "AESDISAB",
+         "AESDTH", "AESHOSP", "AESLIFE", "AESOD", "AEDTC", "AESTDTC", "AEENDTC", "AESTDY", "AEENDY")
---
+++ Part 1 - SDTM/sdtm-exercises/create_dm_exercises.R
@@ -0,0 +1,240 @@
+#' Name: DM domain
+#'
+#' Label: R program to create DM Domain
+#' 
+#' Input 
+#' raw data: 
+#' pharmaverseraw::dm_raw
+#' pharmaverseraw::ec_raw
+#' pharmaverseraw::ds_raw
+#' 
+#' study_controlled_terminology : sdtm_ct.csv
+#'
+library(sdtm.oak)
+library(dplyr)
+
+# Read CT Specification
+study_ct <- read.csv("./Part 1 - SDTM/sdtm-specs/sdtm_ct.csv")
+
+# Read in raw data
+ds_raw <- pharmaverseraw::ds_raw 
+ex_raw <- pharmaverseraw::ec_raw
+dm_raw <- pharmaverseraw::dm_raw
+
+ds_raw <- admiral::convert_blanks_to_na(ds_raw)
+ex_raw <- admiral::convert_blanks_to_na(ex_raw)
+dm_raw <- admiral::convert_blanks_to_na(dm_raw)
+
+# Derive oak_id_vars
+ds_raw <- ds_raw %>%
+  generate_oak_id_vars(
+    pat_var = "PATNUM",
+    raw_src = "ds_raw"
+  )
+
+ex_raw <- ex_raw %>%
+  generate_oak_id_vars(
+    pat_var = "PATNUM",
+    raw_src = "ex_raw"
+  )
+
+dm_raw <- dm_raw %>%
+  generate_oak_id_vars(
+    pat_var = "PATNUM",
+    raw_src = "dm_raw"
+  )
+
+# Create reference dates configuration file
+ref_date_conf_df <- tibble::tribble(
+  ~raw_dataset_name, ~date_var,     ~time_var,      ~dformat,      ~tformat, ~sdtm_var_name,
+  "ex_raw",       "IT.ECSTDAT", NA_character_, "dd-mmm-yyyy", NA_character_,     "RFXSTDTC",
+  "ex_raw",       "IT.ECENDAT", NA_character_, "dd-mmm-yyyy", NA_character_,     "RFXENDTC",
+  "ds_raw",       "IT.DSSTDAT", NA_character_,  "mm-dd-yyyy", NA_character_,      "RFSTDTC",
+  "ex_raw",       "IT.ECENDAT", NA_character_, "dd-mmm-yyyy", NA_character_,      "RFENDTC",
+  "dm_raw",            "IC_DT", NA_character_,  "mm/dd/yyyy", NA_character_,      "RFICDTC",
+  "ds_raw",          "DSDTCOL",     "DSTMCOL",  "mm-dd-yyyy",         "H:M",     "RFPENDTC",
+  "ds_raw",          "DEATHDT", NA_character_,  "mm/dd/yyyy", NA_character_,       "DTHDTC"
+)
+
+# Create DM domain.
+dm <-
+  # Map AGE using assign_no_ct
+  assign_no_ct(
+    raw_dat = dm_raw,
+    raw_var = "IT.AGE",
+    tgt_var = "AGE",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map AGEU using hardcode_ct
+  hardcode_ct(
+    raw_dat = dm_raw,
+    raw_var = "IT.AGE",
+    tgt_var = "AGEU",
+    tgt_val = "Year",
+    ct_spec = study_ct,
+    ct_clst = "C66781",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map SEX using assign_ct
+  assign_ct(
+    raw_dat = dm_raw,
+    raw_var = "IT.SEX",
+    tgt_var = "SEX",
+    ct_spec = study_ct,
+    ct_clst = "C66731",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map ETHNIC using assign_ct
+  assign_ct(
+    raw_dat = dm_raw,
+    raw_var = "IT.ETHNIC",
+    tgt_var = "ETHNIC",
+    ct_spec = study_ct,
+    ct_clst = "C66790",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map RACE using assign_ct
+  assign_ct(
+    raw_dat = dm_raw,
+    raw_var = "IT.RACE",
+    tgt_var = "RACE",
+    ct_spec = study_ct,
+    ct_clst = "C74457",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map ARM using assign_ct
+  assign_ct(
+    raw_dat = dm_raw,
+    raw_var = "PLANNED_ARM",
+    tgt_var = "ARM",
+    ct_spec = study_ct,
+    ct_clst = "ARM",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map ARMCD using assign_no_ct
+  assign_no_ct(
+    raw_dat = dm_raw,
+    raw_var = "PLANNED_ARMCD",
+    tgt_var = "ARMCD",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map ACTARM using assign_ct
+  assign_ct(
+    raw_dat = dm_raw,
+    raw_var = "ACTUAL_ARM",
+    tgt_var = "ACTARM",
+    ct_spec = study_ct,
+    ct_clst = "ARM",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map ACTARMCD using assign_no_ct
+  assign_no_ct(
+    raw_dat = dm_raw,
+    raw_var = "ACTUAL_ARMCD",
+    tgt_var = "ACTARMCD",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map DTHDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "DTHDTC",
+                    min_max = "min",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Map DMDTC using assign_datetime
+  assign_datetime(
+    raw_dat = dm_raw,
+    raw_var = "COL_DT",
+    tgt_var = "DMDTC",
+    raw_fmt = c("m/d/y"),
+    id_vars = oak_id_vars()
+  ) %>%
+  mutate(STUDYID = dm_raw$STUDY,
+         DOMAIN = "DM",
+         SUBJID = substr(dm_raw$PATNUM, 5, 8),
+         USUBJID = paste0("01-", dm_raw$PATNUM),
+         COUNTRY = dm_raw$COUNTRY,
+         DTHFL = dplyr::if_else(is.na(DTHDTC), NA_character_, "Y")) %>%
+  # Derive RFSTDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFSTDTC",
+                    min_max = "min",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Derive RFENDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFENDTC",
+                    min_max = "max",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Derive RFXSTDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFXSTDTC",
+                    min_max = "min",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Derive RFXENDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFXENDTC",
+                    min_max = "max",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Derive RFICDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFICDTC",
+                    min_max = "min",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  # Derive RFPENDTC using oak_cal_ref_dates
+  oak_cal_ref_dates(ds_in = .,
+                    der_var = "RFPENDTC",
+                    min_max = "max",
+                    ref_date_config_df = ref_date_conf_df,
+                    raw_source = list(
+                      ex_raw = ex_raw,
+                      ds_raw = ds_raw,
+                      dm_raw = dm_raw
+                    )
+  ) %>%
+  derive_study_day(
+    sdtm_in = .,
+    dm_domain = .,
+    tgdt = "DMDTC",
+    refdt = "RFXSTDTC",
+    study_day_var = "DMDY"
+  ) %>%
+  select(
+    "STUDYID", "DOMAIN", "USUBJID", "SUBJID",  "RFSTDTC", "RFENDTC", "RFXSTDTC", "RFXENDTC", "RFICDTC", "RFPENDTC", 
+    "DTHDTC", "DTHFL", "AGE", "AGEU", "SEX", "RACE", "ETHNIC", "ARMCD", "ARM", "ACTARMCD", "ACTARM",
+    "COUNTRY", "DMDTC", "DMDY"
+  )
+
